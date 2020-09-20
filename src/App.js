d1577f8ec0d52473b06659acea92eab087918f20
Maria De La Rosa <m.delarosa@gbh.com.do>
2020-09-20 06:59:20 -0400

    feat: add jumbotron styled componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Jumbotron from './components/jumbotron'
+import JumboData from './fixtures/jumbo'
 
 export default function App() {
   return (
     <div className="App">
-     <p>Hello</p>
+     <Jumbotron.Container>
+       {JumboData.map((item)=>(
+         <Jumbotron key={item.id} direction = {item.direction}>
+           <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+           </Jumbotron.Pane>
+           <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+           </Jumbotron.Pane>
+         </Jumbotron>
+       ))}
+     </Jumbotron.Container>
     </div>
   );
 }
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Container, Title, Item, Subtitle, Pane, Image, Inner} from './styles/jumbotron'
+export default function Jumbotron({children, direction = 'row', ...restProps}){
+    return(
+        <Item direction = {direction}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
+    )
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}){
+    return <Container {...restProps}>{children}</Container>
+}
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}){
+    return <Title {...restProps}>{children}</Title>
+}
+Jumbotron.Subtitle = function JumbotronSubtitle({children, ...restProps}){
+    return <Subtitle {...restProps}>{children}</Subtitle>
+}
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}){
+    return <Pane {...restProps}>{children}</Pane>
+}
+Jumbotron.Image = function JumbotronImage({children, ...restProps}){
+    return <Image {...restProps}>{children}</Image>
+}
