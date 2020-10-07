ee46ad971d5a937c430330a4a67cf7b095691022
Maria De La Rosa <m.delarosa@gbh.com.do>
2020-10-07 18:41:06 -0400

    feat: add signup page and firebase integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import{BrowserRouter as Router, Route} from 'react-router-dom';
 import {Home, Browse, Singin, Singup} from './pages/'
+import { IsUserRedirect } from './helpers/routes'
 import * as ROUTES from './constants/routes'
 export default function App() {
   return (
@@ -8,8 +9,11 @@ export default function App() {
         <Route exact path={ROUTES.HOME}>
           <Home />
         </Route>
-        <Route path="/singin">
+        <Route path={ROUTES.SIGN_IN}>
           <Singin />
+        </Route>
+        <Route path={ROUTES.SIGN_UP}>
+          <Singup />
         </Route>
       </Router>
   );
