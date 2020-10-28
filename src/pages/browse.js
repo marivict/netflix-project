ea8be60b12733eacec75c3cdafe1049051508079
Maria De La Rosa <m.delarosa@gbh.com.do>
2020-10-27 22:31:15 -0400

    fix: add semicolon

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {useContent} from '../hooks'
-import {selectionFilter} from '../utils'
-import BrowseContainer from '../containers/browse'
+import {useContent} from '../hooks';
+import {selectionFilter} from '../utils';
+import BrowseContainer from '../containers/browse';
 
 export default function Browser(){
     //we need the series and the films
